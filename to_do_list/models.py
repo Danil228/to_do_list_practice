1ac111d2d6af7f7054d040e27a76e9b6365a8fdb
Danylo <danildacenko@gmail.com>
2023-04-03 11:01:37 +0300

    Created models Tag and Task

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=63)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    done = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tags")
