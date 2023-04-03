dfad6ea8d7341c540e5f7cad21ad08951e999eae
Danylo <danildacenko@gmail.com>
2023-04-03 13:40:15 +0300

    Realized CRUD Views for each model and added task completion confirmation view

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
-from to_do_list.views import TaskListView, TagListView, TagCreateView, TaskCreateView
+from to_do_list.views import TaskListView, TagListView, TagCreateView, TaskCreateView, TagUpdateView, TaskUpdateView, \
+    TagDeleteView, TaskDeleteView, task_completion_confirmation
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="home"),
     path("task/create", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/<int:pk>/confirm_task", task_completion_confirmation, name="task-confirm"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tag/create", TagCreateView.as_view(), name="tag-create"),
+    path("tag/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
+    path("tag/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "to_do_list"
--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,25 +1,61 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
+from to_do_list.forms import TaskForm
 from to_do_list.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
+    paginate_by = 5
 
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
+    success_url = reverse_lazy("to_do_list:home")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("to_do_list:home")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
     success_url = reverse_lazy("to_do_list:home")
 
 
 class TagListView(generic.ListView):
     model = Tag
+    paginate_by = 5
 
 
 class TagCreateView(generic.CreateView):
     model = Tag
     fields = "__all__"
     success_url = reverse_lazy("to_do_list:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("to_do_list:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("to_do_list:home")
+
+
+def task_completion_confirmation(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.done:
+        task.done = False
+    else:
+        task.done = True
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("to_do_list:home"))
